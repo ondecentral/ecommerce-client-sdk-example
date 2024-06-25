21fe81e22006edecaca48f54251db297d19d959d
Ajinkya Rajguru <91156430+akrajguru@users.noreply.github.com>
2024-06-25 16:35:39 -0700

    First commit
    React e-commerce client with luciasdk-t3

---
+++ src/lucia.js
@@ -0,0 +1,9 @@
+import Lucia from "luciasdk-t3";
+
+const lucia = new Lucia({
+  baseURL: "http://localhost:3001",
+  api_key:
+    "60ddfa99-242270db-f9f70533-57cdce5c-44218078-8ea7a864-c8f30ce1-b1b95956",
+});
+
+export default lucia;
