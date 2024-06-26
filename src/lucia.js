5ba805003d12172f08a9949c801b21105365eddc
akrajguru <akrajguru@gmail.com>
2024-06-25 17:14:16 -0700

    added dotenv file

--- src/lucia.js
+++ src/lucia.js
@@ -1,9 +1,8 @@
 import Lucia from "luciasdk-t3";
 
 const lucia = new Lucia({
-  baseURL: "http://localhost:3001",
-  api_key:
-    "60ddfa99-242270db-f9f70533-57cdce5c-44218078-8ea7a864-c8f30ce1-b1b95956",
+  baseURL: process.env.REACT_APP_BASE_URL,
+  api_key: process.env.REACT_APP_API_KEY,
 });
 
 export default lucia;
