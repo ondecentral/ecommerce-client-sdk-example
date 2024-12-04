44c60fbd6444feb20490a9b977dba64adbbe8888
Flow-Biboux <florianesm@gmail.com>
2024-12-04 08:00:35 -0500

    init of tracking

--- src/lucia.js
+++ src/lucia.js
@@ -1,8 +1,7 @@
 import LuciaSDK from "luciasdk-t3";
-
 const lucia = {
   init: () => {
-    LuciaSDK.init({ 
+    LuciaSDK.init({
       baseURL: process.env.REACT_APP_BASE_URL,
       api_key: process.env.REACT_APP_API_KEY,
     });
@@ -13,10 +12,26 @@ const lucia = {
   },
   trackConversion: async (eventTag, amount, eventDetails) => {
     try {
+      console.log("Tracking conversion:", eventTag, amount, eventDetails);
+      // LuciaSDK.buttonClick("conversion-button");
+      // LuciaSDK.init({ api_key, baseURL, clientId, username });
+      // LuciaSDK.userInfo({
+      //   email: "test@test.com",
+      //   firstName: "John",
+      //   lastName: "Doe",
+      // });
       await LuciaSDK.trackConversion(eventTag, amount, eventDetails);
       console.log("Conversion tracked:", eventTag);
     } catch (error) {
       console.error("Tracking conversion failed:", error);
+    }
+  },
+  buttonClick: (buttonId) => {
+    try {
+      console.log("Button clicked:", buttonId);
+      LuciaSDK.buttonClick(buttonId);
+    } catch (error) {
+      console.error("Button click failed:", error);
     }
   },
 };
