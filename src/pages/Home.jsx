b51dc239fba52e628db82c12caa0abd247aa3523
Dmitry Yako <dmtyak@gmail.com>
2026-01-16 20:53:50 +1300

    chore: upgrade sdk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import { Navbar, Main, Product, Footer, ConsentBanner  } from "../components";
 import lucia from "lucia-sdk";
+import { useEffect } from 'react'
 
 function Home() {
-  lucia.pageView("Home");
+  useEffect(() => {
+    lucia.pageView("Home");
+  }, []);
   return (
     <>
       <Navbar />
